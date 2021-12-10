8a572fbc55b5aed384c9351d620195790cf62d07
M Yasir Ismail <yasirismail321@gmail.com>
2021-12-10 18:45:42 +0500

    Changes

---
+++ Videos/design/react-material-admin-master/react-material-admin-master/src/components/HtmlDownlaod/data.js
@@ -0,0 +1,43 @@
+import Img from "../../images/premier.svg";
+import Audio from "../../images/horse.ogg";
+
+export const data = [
+  {
+    name: "Adil Naqvi",
+    title: "Frontend Developer",
+    University: "Quaid-e-Azam Islamabad",
+    position: "1st Position",
+    Image: Img,
+    Audio:
+      "https://www.learningcontainer.com/wp-content/uploads/2020/02/Sample-OGG-File.ogg",
+  },
+  {
+    name: "Khalil Ahmed",
+    title: "Backend Developer",
+    University: "Comsats Islamabad",
+    position: "1st Position",
+    Image: Img,
+    Audio:
+      "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3",
+  },
+  {
+    name: "Talha Hashmi",
+    title: "Backend Developer",
+    University: "Comsats Islamabad",
+    position: "1st Position",
+    Image: Img,
+    Audio:
+      "https://www.learningcontainer.com/wp-content/uploads/2020/02/Sample-OGG-File.ogg",
+  },
+  {
+    name: "Muhammad Yasir Ismail",
+    title: "UI UX Designer",
+    University: "Quaid-e-Azam Islamabad",
+    position: "1st Position",
+    Image: Img,
+    Audio:
+      "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3",
+  },
+];
+
+export const total_students = 10;
